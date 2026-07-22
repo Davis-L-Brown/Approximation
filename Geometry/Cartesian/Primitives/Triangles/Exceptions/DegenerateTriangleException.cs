26652724d535c0356e1dcffde7c0f3702066a1e2
Davis Brown <Davis.Brown@gentex.com>
2026-07-22 14:28:52 -0400

    degenerate triangle exception inherits argumente exception

---
+++ Geometry/Cartesian/Primitives/Triangles/Exceptions/DegenerateTriangleException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Geometry.Cartesian.Primitives.Triangles.Exceptions
+{
+    public class DegenerateTriangleException : ArgumentException
+    {
+        public DegenerateTriangleException(string messsage = "") 
+            : base (messsage) { }
+    }
+}
